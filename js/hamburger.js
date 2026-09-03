5b1072276ce8008ddf7eebc73481119842c25b75
SeaPie <diguld@proton.me>
2026-09-03 20:08:56 +0200

    Haulover

--- js/hamburger.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const hamburger = document.querySelector('.hamburger');
-    const mobileNav = document.querySelector('.mobile-nav');
-    if (hamburger && mobileNav) {
-        hamburger.addEventListener('click', function() {
-            mobileNav.classList.toggle('open');
-        });
-    }
-});
