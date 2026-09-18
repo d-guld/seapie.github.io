1bbe3e8209ebeee9ca3bd2762d3866a1992c8b02
SeaPie <diguld@proton.me>
2026-09-18 22:14:14 +0200

    Halved index.html. Fixed footer position. Changed gradient blur. Removed current-year script.

--- js/current-year.js
+++
@@ -1,9 +0,0 @@
-function setCurrentYear() {
-    const currentYear = new Date().getFullYear();
-    const currentYearElement = document.getElementById("current-year");
-    if (currentYearElement) {
-        currentYearElement.textContent = currentYear;
-    }
-}
-
-document.addEventListener("DOMContentLoaded", setCurrentYear);
